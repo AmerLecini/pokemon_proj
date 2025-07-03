31248afb6cc94426d8552e1eeb39f818a4dd58a8
AmerLecini <amer.lecini@gmail.com>
2025-07-03 15:24:00 +0200

    feat(provider component): add provider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import '@/styles/globals.css';
 import '@/styles/colors.css';
 
 import { siteConfig } from '@/constant/config';
+import { PokemonProvider } from '@/context/PokemonContext';
 
 // !STARTERCONF Change these default meta
 // !STARTERCONF Look at @/constant/config to change them
@@ -56,7 +57,9 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <PokemonProvider>{children}</PokemonProvider>
+      </body>
     </html>
   );
 }
